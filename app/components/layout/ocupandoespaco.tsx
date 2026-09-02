2a0da0b2feec265b9f93f59faa75db6c2c8f5008
Guhss16 <gustavosampaiosamadello@gmail.com>
2026-09-02 18:51:45 -0300

    feat: responsividade da section OcupandoEspaço
    - Configurações de responsividade via breakpoints;
    - Criação de um novo breakpoint (MP com valor de 380px);

--- app/components/layout/ocupandoespaco.tsx
+++ app/components/layout/ocupandoespaco.tsx
@@ -1,7 +1,11 @@
 export default function OcupandoEspaco() {
   return (
-    <div className="mt-[200px] overflow-x-hidden text-white fonteTijolo text-center text-xl leading-[27px] flex flex-col gap-[40px] pb-4">
-      <h2 className="text-[34px] marginPadrao">
+    <div className="mt-[200px] text-white fonteTijolo text-center text-xl leading-[27px] flex flex-col gap-[40px] pb-4 flex flex-col items-center lg:gap-[50px]">
+      <h2
+        className="text-3xl marginPadrao
+      md:text-4xl md:w-[75%] xl:w-[45%] lg:text-5xl
+      "
+      >
         Sua comunicação está{" "}
         <span className="text-[#EE2682]">gerando resultados</span> ou apenas
         <span className="text-[#FEF200] underline font-semibold">
@@ -10,21 +14,21 @@ export default function OcupandoEspaco() {
         </span>
       </h2>
 
-      <div className="bg-[#EE2682] flex flex-row rounded-[100px] relative border-1 border-black items-center marginPadrao">
-        <div className="w-[90%] bg-[#FEF200] text-black py-[10px] border-1 rounded-[100px]">
+      <div className="bg-[#EE2682] flex flex-row rounded-[100px] relative border-1 border-black items-center marginPadrao md:w-[60%]">
+        <div className="w-[90%] bg-[#FEF200] text-black py-[10px] border-1 rounded-[100px] text-lg">
           SUA COMUNICAÇÃO ATUAL
         </div>
-        <p className=" pr-[5px]">100%</p>
+        <p className=" pr-[5px] lg:pr-0">100%</p>
       </div>
 
-      <p className="font-light text-[16px] marginPadrao">
+      <p className="font-light text-lg marginPadrao lg:w-[80%] xl:w-[58%] 2xl:w-[43%]">
         Muitas empresas publicam nas redes sociais sem estratégia ou criam
         campanhas que não engajam as equipes. Sem uma comunicação consistente, a
         marca perde relevância e oportunidades.
       </p>
 
-      <div className="bg-[#383D38] border-1 border-white rotate-357 p-3 w-[120%] ml-[-30px]">
-        <p className="text-white text-[20px] underline">
+      <div className="bg-[#383D38] border-1 border-white rotate-357 p-3 w-[120%] ml-[-30px] sm:ml-[-60px] lg:mt-[10px]">
+        <p className="text-white text-xl md:text-2xl underline">
           A boa notícia é que isso pode mudar.
         </p>
       </div>
