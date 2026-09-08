1360763e6ec8597dc02dca4532fab4fb65141221
Guhss16 <gustavosampaiosamadello@gmail.com>
2026-09-08 15:27:45 -0300

    feat: section OcupandoEspaco com responsividade 100% + Ajuste de fonte

--- app/components/layout/ocupandoespaco.tsx
+++ app/components/layout/ocupandoespaco.tsx
@@ -21,10 +21,15 @@ export default function OcupandoEspaco() {
         <p className=" pr-[5px] lg:pr-0">100%</p>
       </div>
 
-      <p className="font-light text-lg marginPadrao lg:w-[80%] xl:w-[58%] 2xl:w-[43%]">
-        Muitas empresas publicam nas redes sociais sem estratégia ou criam
-        campanhas que não engajam as equipes. Sem uma comunicação consistente, a
-        marca perde relevância e oportunidades.
+      <p className=" text-lg mx-[15px] fonteNoto font-light px-[20px] max-w-[1000px]">
+        Muitas empresas publicam nas redes sociais{" "}
+        <span className="font-extrabold underline uppercase px-1">
+          sem estratégia
+        </span>{" "}
+        ou criam campanhas que{" "}
+        <span className="font-extrabold underline uppercase ">não engajam</span>{" "}
+        as equipes. Sem uma comunicação consistente, a marca perde relevância e
+        oportunidades.
       </p>
 
       <div className="bg-[#383D38] border-1 border-white rotate-357 p-3 w-[120%] sm:ml-[-60px] lg:mt-[10px]">
