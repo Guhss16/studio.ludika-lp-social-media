1708253304cad6fb3a763e0c78fdf0c6542f39df
Guhss16 <gustavosampaiosamadello@gmail.com>
2026-09-16 14:45:35 -0300

    style: ajuste de cores do footer

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -15,10 +15,10 @@ export default function Footer() {
   return (
     <footer className="pt-[40px] bg-[#383D38] mt-[50px] border-t-2">
       {" "}
-      <div className="flex flex-col justify-center items-center gap-[20px]">
+      <div className="flex flex-col justify-center items-center gap-[30px]">
         {/*Logo*/}
         <a
-          className="cursor-pointer bg-[#DCDDC3] py-2 px-4 rounded-full"
+          className="cursor-pointer bg-[#DCDDC3] py-2 px-4 rounded-full shadow-[0px_25px_16px_-3px_rgba(0,_0,_0,_0.45)]"
           href="/"
         >
           <LogoPreto className="w-[150px]" height="auto" />
@@ -31,7 +31,7 @@ export default function Footer() {
             <a
               key={item.href}
               href={item.href}
-              className="relative text-black font-semibold text-[16px] transition-colors duration-200 group"
+              className="relative text-white uppercase fonteTijolo font-normal  text-[16px] transition-colors duration-200 group underline tracking-[2px]"
             >
               {item.label}
               <span className="absolute -bottom-1 left-1/2 -translate-x-1/2 w-0 h-px bg-[#e72579] transition-all duration-300 group-hover:w-full" />{" "}
@@ -44,7 +44,7 @@ export default function Footer() {
         {/*Politica de Privacidade */}
         <a
           href=""
-          className="text-black font-semibold text-[16px]  transition-colors duration-200 group underline"
+          className="fonteTijolo font-normal text-white uppercase text-[16px] transition-colors duration-200 group underline tracking-[2px]"
         >
           Politica de Privacidade
         </a>
